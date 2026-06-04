2540cba0dae4294c4d4ad47c3d6ac2880d0894b8
Priyanshu Kumar <priyanshukumar55a@gmail.com>
2026-06-05 04:17:07 +0530

    done initial setup

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
-
+const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
+const server = http.createServer(app);
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <h1 className="text-4xl font-bold text-center">PulseChat</h1>;
+}
+
+export default App;
