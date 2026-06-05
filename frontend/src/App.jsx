7cbcc82ccfb4cb83273a9f9ef5f46d9bb3e80e24
Priyanshu Kumar <priyanshukumar55a@gmail.com>
2026-06-06 01:24:18 +0530

    implemented schemas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
-  return <h1 className="text-4xl font-bold text-center">PulseChat</h1>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/chats" element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
