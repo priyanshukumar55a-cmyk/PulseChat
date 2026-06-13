c1d3e6dac2a85ba7996ff72916ede3e06307d332
Priyanshu Kumar <priyanshukumar55a@gmail.com>
2026-06-14 04:52:26 +0530

    feat: Rename, Add, Remove from a group chat

--- frontend/src/miscellaneous/UpdateGrpChatModal.jsx
+++ frontend/src/miscellaneous/UpdateGrpChatModal.jsx
@@ -6,15 +6,18 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Pencil } from "lucide-react";
+import { Loader2, Pencil } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { ChatState } from "@/context/chatProvider";
 import { Input } from "@/components/ui/input";
 import api from "@/api/axios";
 import { toast } from "sonner";
+import { Avatar,AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useAuth } from "@/context/AuthContext";
 
 const UpdateGrpChatModal = () => {
+  const{user} = useAuth()
   const [open, setOpen] = useState(false);
 
   const [grpChatName, setGrpChatName] = useState("");
@@ -26,17 +29,37 @@ const UpdateGrpChatModal = () => {
 
   const { chats, setChats, selectedChat, setSelectedChat } = ChatState();
 
-  const handleRemoveUser = () => {};
+    const handleRemoveUser = async (userToRemove) => {
+        
+        if (selectedChat.groupAdmin._id !== user._id && userToRemove._id !== user._id) {
+            return toast.warning("Only admins can remove someone!");
+        }
+
+        try {
+            setLoading(true);
+            const { data } = await api.put("chat/groupRemove", {
+                chatId: selectedChat._id,
+                userId: userToRemove._id,
+            })
+
+            toast.success("User removed successfully");
+            userToRemove._id === user._id ? setSelectedChat() : setSelectedChat(data);
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Error occurred!");
+        } finally {
+            setLoading(false)
+        }
+  };
   const handleRename = async () => {
     try {
       setRenameLoading(true);
-      const { data } = api.put("/chat/rename", {
+      const { data } = await api.put("/chat/rename", {
         chatId: selectedChat._id,
         chatName: grpChatName,
       });
 
-        setSelectedChat(data);
-        toast.success("Group name updated successfully")
+      setSelectedChat(data);
+      toast.success("Group name updated successfully");
     } catch (error) {
       toast.error(error.response?.data?.message || "Error occurred!");
     } finally {
@@ -45,7 +68,48 @@ const UpdateGrpChatModal = () => {
 
     setGrpChatName("");
   };
-  const handleSearch = () => {};
+  const handleSearch = async (query) => {
+    setSearch(query);
+    if (!query) {
+      setSearchResult([]);
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const { data } = await api.get(`/user?search=${query}`);
+      setSearchResult(data);
+    } catch (error) {
+      toast.error("Falied to Load the Search Results");
+    } finally {
+      setLoading(false);
+    }
+  };
+    const handleAddUser = async (userToAdd) => {
+        if (selectedChat.users.find((u) => u._id === userToAdd._id)) {
+          return toast.warning("User already in group!")
+        }
+        
+        if (selectedChat.groupAdmin._id !== user._id) {
+            return toast.warning("Only admins can add someone!");
+        }
+
+        try {
+            setLoading(true);
+            const { data } = await api.put("chat/groupAdd", {
+                chatId: selectedChat._id,
+                userId: user._id,
+            })
+            toast.success("User added successfully");
+            setSelectedChat(data)
+            setSearch("")
+            setSearchResult([])
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Error occurred!");
+        } finally {
+            setLoading(false)
+        }
+  };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -97,8 +161,44 @@ const UpdateGrpChatModal = () => {
           onChange={(e) => handleSearch(e.target.value)}
           autoComplete="new-password"
         />
+        {loading && (
+          <div className="flex justify-center">
+            <Loader2 className="h-6 w-6 animate-spin" />
+          </div>
+        )}
+        {search.length > 0 && searchResult.length == 0 && !loading ? (
+          <button
+            type="button"
+            className="w-full flex items-center gap-3 px-4 py-2 text-left transition border-2 rounded-md border-black/15 hover:cursor-pointer bg-cyan-300 hover:bg-cyan-200"
+          >
+            No Users Found
+          </button>
+        ) : (
+          searchResult?.slice(0, 4).map((user) => {
+            return (
+              <button
+                key={user._id}
+                type="button"
+                onClick={() => handleAddUser(user)}
+                className="w-full flex items-center gap-3 px-4 py-2 text-left transition border-2 rounded-md border-black/15 hover:cursor-pointer bg-cyan-300 hover:bg-cyan-200"
+              >
+                <Avatar className="h-8 w-8">
+                  <AvatarImage src={user?.pic} alt={user?.username} />
+                  <AvatarFallback>
+                    {user?.username?.[0]?.toUpperCase() || "U"}
+                  </AvatarFallback>
+                </Avatar>
+
+                <div>
+                  <p className="font-medium">{user.username}</p>
+                  <p className="text-xs text-muted-foreground">{user.email}</p>
+                </div>
+              </button>
+            );
+          })
+        )}
         <Button
-          onClick={() => handleRemoveUser(u)}
+          onClick={() => handleRemoveUser(user)}
           className="bg-red-600 hover:bg-red-700 cursor-pointer px-4 py-5"
         >
           {renameLoading ? "Leaving..." : "Leave Group"}
